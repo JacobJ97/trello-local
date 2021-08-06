8af34cb9a09791a3f96c952e52f539eee619f1a7
JacobJ97 <jacobjackson977@hotmail.com>
2021-08-07 07:50:12 +1000

    Adding Draggable tags and functions, to signify that these components are the ones being dragged. Updating relevant information to ensure it all works.

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -3,8 +3,9 @@ import Button from './Button';
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import EditField from './EditField';
+import { Draggable } from 'react-beautiful-dnd';
 
-const Task = ({task, section, deleteTask, modalActive, editTask}) => {
+const Task = ({task, section, deleteTask, modalActive, editTask, index}) => {
 
     const [editTaskTitle, setEditTaskTitle] = useState('');
     const [editTaskDescription, setEditTaskDescription] = useState('');
@@ -22,7 +23,7 @@ const Task = ({task, section, deleteTask, modalActive, editTask}) => {
             return;
         }
 
-        editTask(task.task_id, "task_name", "title", editTaskTitle)
+        editTask(task.task_id, "task_name", "title", editTaskTitle, task.SectionSectionId)
         setTaskToggleTitle(false);
         setEditTaskTitle('');
     }
@@ -35,7 +36,7 @@ const Task = ({task, section, deleteTask, modalActive, editTask}) => {
             return;
         }
 
-        editTask(task.task_id, "task_description", "description", editTaskDescription)
+        editTask(task.task_id, "task_description", "description", editTaskDescription, task.SectionSectionId)
         setTaskToggleDescription(false);
         setEditTaskDescription('');
     }
@@ -48,22 +49,26 @@ const Task = ({task, section, deleteTask, modalActive, editTask}) => {
             return;
         }
 
-        editTask(task.task_id, "task_labels", "labels", editTaskLabels)
+        editTask(task.task_id, "task_labels", "labels", editTaskLabels, task.SectionSectionId)
         setTaskToggleLabels(false);
         setEditTaskLabels('');
     }
-    
+
     return (
         <>
         { task.SectionSectionId === section.section_id && (
-        <div style={{backgroundColor: 'orange'}} className={"task task-item-" + task.task_id}>
-            <div className="task-heading">
-                {taskToggleTitle ? <EditField className="edit-task-title" onSubmit={onSubmitTitle} value={editTaskTitle} placeholder={task.task_name} onChange={(e) => setEditTaskTitle(e.target.value)} /> : <h2 onDoubleClick={() => setTaskToggleTitle(!taskToggleTitle)}>{task.task_name}</h2> }
-                <Button iconText={faTimes} classText="btn btn-times" onClick={() => deleteTask(task.task_id)} disabled={modalActive} />
-            </div>    
-            {taskToggleDescription ? <EditField className="edit-task-desc" onSubmit={onSubmitDesc} value={editTaskDescription} placeholder={task.task_description} onChange={(e) => setEditTaskDescription(e.target.value)} /> : <p onDoubleClick={() => setTaskToggleDescription(!taskToggleDescription)}>{task.task_description}</p> }
-            {taskToggleLabels ? <EditField className="edit-task-labels" onSubmit={onSubmitLabels} value={editTaskLabels} placeholder={task.task_labels} onChange={(e) => setEditTaskLabels(e.target.value)} /> : <span onDoubleClick={() => setTaskToggleLabels(!taskToggleLabels)}>{task.task_labels}</span> }
-        </div>
+            <Draggable key={task.task_id} draggableId={'draggable-' + task.task_id} index={index}>
+            {(provided, snapshot) => (    
+                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} data-id={task.task_id} data-order={task.task_order} style={{backgroundColor: 'orange', ...provided.draggableProps.style}} className={"task task-item-" + task.task_id}>
+                    <div className="task-heading">
+                        {taskToggleTitle ? <EditField className="edit-task-title" onSubmit={onSubmitTitle} value={editTaskTitle} placeholder={task.task_name} onChange={(e) => setEditTaskTitle(e.target.value)} /> : <h2 onDoubleClick={() => setTaskToggleTitle(!taskToggleTitle)}>{task.task_name}</h2> }
+                        <Button iconText={faTimes} classText="btn btn-times" onClick={() => deleteTask(task.task_id, task.SectionSectionId, task.task_order)} disabled={modalActive} />
+                    </div>    
+                    {taskToggleDescription ? <EditField className="edit-task-desc" onSubmit={onSubmitDesc} value={editTaskDescription} placeholder={task.task_description} onChange={(e) => setEditTaskDescription(e.target.value)} /> : <p onDoubleClick={() => setTaskToggleDescription(!taskToggleDescription)}>{task.task_description}</p> }
+                    {taskToggleLabels ? <EditField className="edit-task-labels" onSubmit={onSubmitLabels} value={editTaskLabels} placeholder={task.task_labels} onChange={(e) => setEditTaskLabels(e.target.value)} /> : <span onDoubleClick={() => setTaskToggleLabels(!taskToggleLabels)}>{task.task_labels}</span> }
+                </div>
+            )}
+            </Draggable>
         ) }
         </>
     )
