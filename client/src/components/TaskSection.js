2eb4b4e90a9398cd1ba3d9ad1ff29fe989924084
JacobJ97 <jacobjackson977@hotmail.com>
2021-08-07 07:50:53 +1000

    Adding Droppable tags and functions, to show that Sections are where Tasks are being dropped into after being dragged. Updating relevant code to ensure it all works.

--- client/src/components/TaskSection.js
+++ client/src/components/TaskSection.js
@@ -4,8 +4,9 @@ import Tasks from './Tasks';
 import EditField from './EditField';
 import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
+import { Droppable } from 'react-beautiful-dnd';
 
-const TaskSection = ({section, tasks, onAddModal, modalActive, deleteSection, deleteTask, editSection, editTask}) => {
+const TaskSection = ({section, tasks, onAddModal, modalActive, deleteSection, deleteTask, editSection, editTask, provided, snapshot}) => {
 
     const [sectionNameEdit, setSectionNameEdit] = useState('');
 
@@ -25,17 +26,22 @@ const TaskSection = ({section, tasks, onAddModal, modalActive, deleteSection, de
     }
 
     return (
-        <div data-key={section.section_id} className="section">
-            <div className="section-heading">
-                <Button iconText={faPlus} classText="btn btn-plus" onClick={() => onAddModal(section.section_id)} disabled={modalActive} />
-                {sectionToggle ? 
-                <EditField className="edit-section-name" onSubmit={onSubmit} value={sectionNameEdit} placeholder={section.section_name} onChange={(e) => setSectionNameEdit(e.target.value)} />
-                : 
-                <h1 onDoubleClick={() => setSectionToggle(!sectionToggle)}>{section.section_name}</h1>}
-                <Button iconText={faTimes} classText="btn btn-times" onClick={() => deleteSection(section.section_id)} disabled={modalActive} />
+        <Droppable key={section.section_id} droppableId={'' + section.section_id}>
+        {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps} data-key={section.section_id} className={"section section-id-" + section.section_id}>
+                <div className="section-heading">
+                    <Button iconText={faPlus} classText="btn btn-plus" onClick={() => onAddModal(section.section_id, document.querySelectorAll('.section-id-' + section.section_id + ' .task').length+1)} disabled={modalActive} />
+                    {sectionToggle ? 
+                    <EditField className="edit-section-name" onSubmit={onSubmit} value={sectionNameEdit} placeholder={section.section_name} onChange={(e) => setSectionNameEdit(e.target.value)} />
+                    : 
+                    <h1 onDoubleClick={() => setSectionToggle(!sectionToggle)}>{section.section_name}</h1>}
+                    <Button iconText={faTimes} classText="btn btn-times" onClick={() => deleteSection(section.section_id)} disabled={modalActive} />
+                </div>
+                <Tasks tasks={tasks} section={section} deleteTask={deleteTask} modalActive={modalActive} editTask={editTask} />
+                {provided.placeholder}
             </div>
-            <Tasks tasks={tasks} section={section} deleteTask={deleteTask} modalActive={modalActive} editTask={editTask} />
-        </div>
+        )}
+        </Droppable>
     )
 }
 
